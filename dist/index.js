ec4c1280449fa97460871c8261b527ffcf36d9fa
Donghyeok Shin <smdh1010@gmail.com>
2024-11-20 03:28:36 +0900

    feature: add random-korean-nickname.d.ts

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+import { firstWords } from "./firstWords";
+import { secondWords } from "./secondWords";
+import { thirdWords } from "./thirdWords";
+export function newRandomNick(customFirstWords, customSecondWords, customThirdWords) {
+    const firstWord = pickOne(customFirstWords || firstWords);
+    const secondWord = pickOne(customSecondWords || secondWords);
+    const thirdWord = pickOne(customThirdWords || thirdWords);
+    return `${firstWord}${secondWord}${thirdWord}`;
+}
+function pickOne(words) {
+    return words[Math.floor(Math.random() * words.length)];
+}
