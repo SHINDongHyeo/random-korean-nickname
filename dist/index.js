0cf63d1fbebd6769cf47e841480ce33677bb5e95
Donghyeok Shin <smdh1010@gmail.com>
2024-11-19 23:04:59 +0900

    fix: newRnadomNick func --> default

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
-export function newRandomNick(length = 8) {
-    const characters = "가나다라마바사아자차카타파하";
-    let result = "";
-    for (let i = 0; i < length; i++) {
-        result += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-    return result;
+export default function newRandomNick(length = 8) {
+  const characters = "가나다라마바사아자차카타파하";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
 }
