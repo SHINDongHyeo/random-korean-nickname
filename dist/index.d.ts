a5b4c2999c532e66ff1e75ef7ffab03bfb9b09b4
Donghyeok Shin <smdh1010@gmail.com>
2024-11-19 21:54:09 +0900

    Init

---
+++ dist/index.d.ts
@@ -0,0 +1,1 @@
+export declare function newRandomNick(length?: number): string;
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export function newRandomNick(length: number = 8): string {
+  const characters = "가나다라마바사아자차카타파하";
+  let result = "";
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
