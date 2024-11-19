39cd7ff314b85cda9bfc9e4e910c0427727240d3
Donghyeok Shin <smdh1010@gmail.com>
2024-11-20 02:33:04 +0900

    feature: add CommonJS ( .mjs + .cjs)

---
+++ dist/index.cjs
@@ -0,0 +1,16 @@
+const { firstWords } = require("./firstWords");
+const { secondWords } = require("./secondWords");
+const { thirdWords } = require("./thirdWords");
+export function newRandomNick(
+  customFirstWords,
+  customSecondWords,
+  customThirdWords
+) {
+  const firstWord = pickOne(customFirstWords || firstWords);
+  const secondWord = pickOne(customSecondWords || secondWords);
+  const thirdWord = pickOne(customThirdWords || thirdWords);
+  return `${firstWord}${secondWord}${thirdWord}`;
+}
+function pickOne(words) {
+  return words[Math.floor(Math.random() * words.length)];
+}
