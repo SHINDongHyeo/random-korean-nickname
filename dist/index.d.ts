3b8b4e2c2673e533d7b4d1c83ac22976c7a54e69
Donghyeok Shin <smdh1010@gmail.com>
2025-03-23 21:07:46 +0900

    feat: cjs, mjs에러 수정

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,11 @@
-export declare function newRandomNick(customFirstWords?: Array<string>, customSecondWords?: Array<string>, customThirdWords?: Array<string>): string;
+declare module "random-korean-nickname" {
+  export declare function newRandomNick(
+    customFirstWords?: Array<string>,
+    customSecondWords?: Array<string>,
+    customThirdWords?: Array<string>
+  ): string;
+
+  export const firstWords: string[];
+  export const secondWords: string[];
+  export const thirdWords: string[];
+}
